573f8205b289fd13dd37e7f0134a8ad57d6d2e27
Zafar1715 <doni11735@gmail.com>
2026-05-11 18:44:17 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 TOKEN = "8658556905:AAEQfrHltU9yCFE2b45jmmHXjB61enYwGvY"
 
-ADMINS = [123456789]       # твой Telegram ID
+ADMINS = [8676281750]       # твой Telegram ID
 DISPATCHERS = [123456789]  # кто может вводить
